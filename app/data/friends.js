fc776eb011b34e0b7c4303fbbfbf57d74d8ae870
Nick F <nick.ferrazza92@gmail.com>
2018-10-11 14:35:26 -0400

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,47 +3,35 @@ var friends = [
         "name": "Ursula",
         "image": "https://upload.wikimedia.org/wikipedia/commons/8/86/Ahmed_I_by_John_Young_%28cropped%29.jpg",
         "scores": [
+            "2",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1"
+            "4",
+            "5",
+            "3",
+            "2"
         ]
     },
     {
         "name": "Hades",
         "image": "http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/thumbs/hades-hercules-1.15_thumb.jpg",
         "scores": [
-            "2",
-            "2",
-            "2",
-            "2",
-            "2",
-            "2",
-            "2",
-            "2",
-            "2",
-            "2"
+            "3",
+            "4",
+            "1",
+            "3",
+            "4",
+            "1"
         ]
     },
     {
         "name": "Gaston",
         "image": "http://www.whatsitsgalore.com/disney/villain-art/villain-gaston.jpg",
         "scores": [
+            "4",
             "3",
-            "3",
-            "3",
-            "3",
-            "3",
-            "3",
-            "3",
-            "3",
-            "3",
+            "5",
+            "1",
+            "2",
             "3"
         ]
     },
@@ -51,15 +39,11 @@ var friends = [
         "name": "Cruella",
         "image": "http://www.geocities.ws/viloesmdg/cruella.jpg",
         "scores": [
+           "5",
+           "5",
+           "3",
            "4",
-           "4",
-           "4",
-           "4",
-           "4",
-           "4",
-           "4",
-           "4",
-           "4",
+           "1",
            "4"
         ]
     },
@@ -67,31 +51,11 @@ var friends = [
         "name": "Queen of Hearts",
         "image": "http://img1.wikia.nocookie.net/__cb20120627174836/aliceinwonderland/images/thumb/0/05/Q4.jpg/185px-Q4.jpg",
         "scores": [
-           "5",
-           "5",
-           "5",
-           "5",
-           "5",
-           "5",
-           "5",
-           "5",
-           "5",
-           "5"
-        ]
-    },
-    {
-        "name": "Jafar",
-        "image": "http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/thumbs/jafar-aladdin-4.49_thumb.jpg",
-        "scores": [
-           "5",
-           "4",
-           "3",
-           "2",
-           "1",
            "1",
            "2",
+           "2",
            "3",
-           "4",
+           "5",
            "5"
         ]
     }
